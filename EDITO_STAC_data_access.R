58cc78c1848e73e1157c303de317da76aed8e2e8
lottepohl <lotte.pohl@vliz.be>
2026-01-22 08:16:36 +0000

    check push rights

--- EDITO_STAC_data_access.R
+++ EDITO_STAC_data_access.R
@@ -3,7 +3,7 @@ library(rstac)
 library(purrr)
 library(arrow)
 library(dplyr)
-
+library(leaflet)
 
 # acces the STAC catalogue and get collections ----------------------------
 
@@ -25,11 +25,11 @@ c_all <- col_obj$collections |> vapply(`[[`, character(1), "id") %>% as_tibble()
 ## collection names
 c_ETN_data <- "animal_tracking_datasets"
 
-c_bathy_list <- c_all %>% dplyr::filter(grepl("bathymetry", col_all$value))
+c_bathy_list <- c_all %>% dplyr::filter(grepl("bathymetry", c_all$value))
 c_bathy_selection <- "emodnet-bathymetry"
 
 c_owf_list <- c_all %>% dplyr::filter(grepl("wind", c_all$value))
-c_owf_selection <- c("emodnet-wind_farm_area_sq_km", "emodnet-wind_farm_year")
+c_owf_selection <- c("emodnet-wind_farm_year")
 
 #col_shipwrecks <- col_all %>% dplyr::filter(grepl("ship_wreck", col_all$value))
 
@@ -52,10 +52,41 @@ etn_items <- stac_obj %>%
 
 etn_link_href <- etn_items$features[[13]]$assets$data$href
 
-arrow::open_dataset(etn_link_href, format = "parquet")
+etn_seabass <- arrow::read_parquet(etn_link_href, format = "parquet")
 
-arrow::open_dataset("https://www.lifewatch.be/etn/parquet/detections/PhD_Goossens/PhD_Goossens_detections", format = "parquet")
 
+# OWF ---------------------------------------------------------------------
+
+OWF_col <- stac_obj %>%
+  collections(c_owf_selection)%>%
+  get_request()
+
+owf_items <- stac_obj %>%
+  stac_search(collections = c_owf_selection, limit = 500) %>%
+  get_request() %>%
+  items_fetch()
+
+# reading parquet does not work!
+owf_link_parquet_href <- owf_items$features[[1]]$assets$parquet$href
+owf <- arrow::read_parquet(owf_link_wmf_href, format = "parquet")
+
+# wms
+owf_link_wmf_href <- owf_items$features[[1]]$assets$wms$href
+
+wms_base <- "https://ows.emodnet-humanactivities.eu/wms" #TODO: make programmatically
+layer_name <- "windfarmspoly" #TODO: make programmatically
+
+leaflet() %>% #TODO: understand what colors mean
+  addProviderTiles("CartoDB.Positron") %>%
+  addWMSTiles(
+    baseUrl = wms_base,
+    layers  = layer_name,
+    options = WMSTileOptions(
+      format = "image/png",
+      transparent = TRUE
+      # You can add: opacity = 0.7
+    )
+  )
 
 # bathymetry --------------------------------------------------------------
 
@@ -72,6 +103,80 @@ test <- bathy_items$features[[20]]
 bathy_link_href <- bathy_items$features[[20]]$assets$data$href
 
 arrow::open_dataset(test[["url"]])
+
+rstac::assets_url(test)                 # all asset hrefs
+
+
+# chatgpt suggestions -----------------------------------------------------
+
+library(rstac)
+library(purrr)
+library(dplyr)
+library(leaflet)
+
+# bathy_items is your rstac items object
+
+# 1) Find an item that actually has a WMS asset
+has_asset <- function(item, asset_name) {
+  !is.null(item$assets[[asset_name]]) && !is.null(item$assets[[asset_name]]$href)
+}
+
+idx_wms <- which(map_lgl(bathy_items$features, has_asset, asset_name = "wms"))[1]
+
+stopifnot(!is.na(idx_wms))  # will error if no WMS found
+
+wms_item <- bathy_items$features[[idx_wms]]
+wms_href <- wms_item$assets$wms$href
+wms_href
+# print it so you can inspect the URL
+
+# 2) Add it in leaflet
+# Leaflet needs:
+#   - a base WMS URL (usually the part before the '?')
+#   - one or more layer names (often you can discover these from GetCapabilities)
+wms_base <- sub("\\?.*$", "", wms_href)
+
+# GetCapabilities URL (helps you discover the 'layers' name)
+caps_url <- paste0(wms_base, "?service=WMS&request=GetCapabilities")
+browseURL(caps_url)
+
+# 3) Replace this with a real layer name you see in GetCapabilities
+layer_name <- "emodnet:mean"
+layer_name <- "emodnet:mean_multicolour"
+layer_name <- "mean_atlas_land"
+
+leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addWMSTiles(
+    baseUrl = wms_base,
+    layers  = layer_name,
+    options = WMSTileOptions(format = "image/png", transparent = TRUE)
+  )
+
+
+# geotiff -----------------------------------------------------------------
+
+library(terra)
+library(leaflet)
+
+idx_tif <- which(map_lgl(bathy_items$features, has_asset, asset_name = "geotiff"))[1]
+tif_href <- bathy_items$features[[idx_tif]]$assets$geotiff$href
+
+# download (recommended for https) then read
+tf <- tempfile(fileext = ".tif")
+download.file(tif_href, tf, mode = "wb")
+
+r <- terra::rast(tf)
+
+leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addRasterImage(r, opacity = 0.7)
+
+
+# other -------------------------------------------------------------------
+
+
+
 # bathy_href <- bathy_objects[["url"]]
 # arrow::open_dataset(bathy_href)
 
@@ -95,7 +200,7 @@ shipwreck <- bathy_items %>% dplyr::filter(grepl(shipwreck_projectid, bathy_item
 #   format = "parquet"
 # )
 # return(eurobis)
-}
+#}
 # Do this when: This is a direct connection to the S3 bucket, using Arrow’s S3FileSystem.
 # 
 # Advantages:
